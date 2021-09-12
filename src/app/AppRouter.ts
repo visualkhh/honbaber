22ad576f1fbace2258063aa64ccf8834b24806df
khh <visualkhh@gmail.com>
2021-09-12 13:09:50 +0900

    initialize

--- src/app/AppRouter.ts
+++ src/app/AppRouter.ts
@@ -6,6 +6,10 @@ import css from './layouts/app.css'
 import { RouterAction } from 'simple-boot-core/route/RouterAction';
 import { Intent } from 'simple-boot-core/intent/Intent';
 import { ConstructorType } from 'simple-boot-core/types/Types';
+import { FrontLifeCycle } from 'simple-boot-front/module/FrontLifeCycle';
+
+declare var naver: any;
+
 @Sim({scheme: 'layout-router'})
 @Router({
     path: '',
@@ -15,10 +19,30 @@ import { ConstructorType } from 'simple-boot-core/types/Types';
     }
 })
 @Component({template, styles: [css]})
-export class AppRouter implements RouterAction {
+export class AppRouter implements RouterAction, FrontLifeCycle {
+
     child?: any;
+
+    onCreate(): void {
+        // throw new Error('Method not implemented.');
+    }
+
     canActivate(url: Intent, module: any): void {
-        console.log('->>>>>', this, module)
-        this.child = module;
+        // console.log('->>>>>', this, module)
+        // this.child = module;
+    }
+
+    onChangedRender(): void {
+    }
+
+    onFinish(): void {
+    }
+
+    onInit(): void {
+        var mapDiv = document.getElementById('map'); // 'map'으로 선언해도 동일
+        var map = new naver.maps.Map(mapDiv);
+    }
+
+    onInitedChild(): void {
     }
 }
--- src/app/features/index/index.ts
+++ src/app/features/index/index.ts
@@ -6,10 +6,12 @@ import { Sim } from 'simple-boot-core/decorators/SimDecorator';
 import { SimstanceManager } from 'simple-boot-core/simstance/SimstanceManager';
 import { RandomUtils } from 'simple-boot-core/utils/random/RandomUtils';
 import { Component } from 'simple-boot-front/decorators/Component';
+import { FrontLifeCycle } from 'simple-boot-front/module/FrontLifeCycle';
+
 
 @Sim({scheme: 'index'})
 @Component({template, styles: [css]})
-export class Index {
+export class Index implements FrontLifeCycle {
     data = 'hello simple-boot-front';
     randomData = 99;
     randomColorData = '#000000';
@@ -17,7 +19,11 @@ export class Index {
     constructor(public testService: TestService, public manager: SimstanceManager, public navigation: Navigation) {
     }
 
+    onCreate(): void {
+    }
+
     onInit() {
+
     }
 
     random() {
@@ -26,5 +32,17 @@ export class Index {
 
     randomColor() {
         this.randomColorData = RandomUtils.getRandomColor();
+    }
+
+    onChangedRender(): void {
+        alert(1)
+    }
+
+    onFinish(): void {
+        alert(1)
+    }
+
+    onInitedChild(): void {
+        alert(1)
     }
 }
