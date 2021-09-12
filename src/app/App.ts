9312d41770aa4ec5fffce5457e0baac1911941bd
khh <visualkhh@gmail.com>
2021-09-12 16:11:35 +0900

    initialize

--- src/app/App.ts
+++ src/app/App.ts
@@ -3,12 +3,16 @@ import { Component } from 'simple-boot-front/decorators/Component';
 import template from './app.html'
 import css from './app.css'
 import { FrontLifeCycle } from 'simple-boot-front/module/FrontLifeCycle';
+import { ProjectService } from './services/ProjectService';
 
 declare var naver: any;
 
 @Sim({scheme: 'layout-router'})
 @Component({template, styles: [css]})
 export class App implements FrontLifeCycle {
+
+    constructor(public projectService: ProjectService) {
+    }
 
     onCreate(): void {
     }
@@ -19,7 +23,9 @@ export class App implements FrontLifeCycle {
     onFinish(): void {
     }
 
-    onInit(): void {
+    async onInit() {
+        const data = await this.projectService.loadScript('https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=83bfuniegk&amp;submodules=panorama,geocoder,drawing,visualization')
+        console.log(data);
         new naver.maps.Map(document.querySelector('#map'));
     }
 
---
+++ src/app/services/ProjectService.ts
@@ -0,0 +1,26 @@
+import {Sim} from 'simple-boot-core/decorators/SimDecorator';
+
+@Sim()
+export class ProjectService {
+    constructor() {
+    }
+
+    public calc(i: number, y: number) {
+        return i + y
+    }
+
+    public tail(origin: string | number, tail: string) {
+        return origin + tail;
+    }
+
+    public loadScript(src: string) {
+        return new Promise((resolve, reject) => {
+            const script = document.createElement('script')
+            script.type = 'text/javascript'
+            script.onload = resolve
+            script.onerror = reject
+            script.src = src
+            document.head.append(script)
+        })
+    }
+}
